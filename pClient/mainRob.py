b6b5c049db0b1c4068520fa77d944fcc41f08c2a
daniel85111 <danielba@ua.pt>
2022-11-25 19:53:55 +0000

    Creation

---
+++ pClient/mainRob.py
@@ -0,0 +1,298 @@
+
+import sys
+from croblink import *
+from math import *
+import xml.etree.ElementTree as ET
+#import particles
+import particleFilter
+import numpy as np
+import random
+import cv2
+
+CELLROWS=7
+CELLCOLS=14
+
+class MyRob(CRobLinkAngs):
+    def __init__(self, rob_name, rob_id, angles, host):
+        CRobLinkAngs.__init__(self, rob_name, rob_id, angles, host)
+        self.rob_name = rob_name
+          
+        self.motors = (0.0,0.0)
+        self.last_motors = (0.0,0.0)
+
+        # Position
+        self.firstRun = 1
+        self.initialx = None
+        self.initialy = None
+        self.x = None
+        self.y = None
+        self.posx = None
+        self.posy = None
+        self.x_od_pos = 0
+        self.y_od_pos = 0
+        self.ori= 0
+        self.robot_diameter = 1
+        self.border = None
+
+        self.x_posmin = 0
+        self.y_posmin = 0
+        self.x_posmax = 0
+        self.y_posmax = 0
+
+
+        # Lista de particulas
+        self.mapmax_x = 28
+        self.mapmax_y=14
+        self.particulas = particleFilter.filtroParticulas()
+        self.i = 0
+        self.inputfilter = []
+        
+        
+        
+        # Tamanho imagem = 28 x 14 -> 1120 x 560 = simulação*40         x40  
+        self.immax_x = 1120
+        self.immax_y = 560
+        self.img = np.zeros((self.immax_y,self.immax_x,3), np.uint8)
+
+    def setMap(self, labMap):
+        self.labMap = labMap
+
+    def printMap(self):
+        for l in reversed(self.labMap):
+            print(''.join([str(l) for l in l]))
+
+    def run(self):
+        if self.status != 0:
+            print("Connection refused or error")
+            quit()
+
+        state = 'stop'
+        stopped_state = 'run'
+
+        while True:
+            self.readSensors()
+            if self.measures.gpsReady:
+                self.x = self.measures.x
+                self.y = self.measures.y
+            
+            if self.measures.compassReady:
+                self.ori= self.measures.compass
+            
+            if self.measures.endLed:
+                print(self.rob_name + " exiting")
+                quit()
+
+            if state == 'stop' and self.measures.start:
+                state = stopped_state
+
+            if state != 'stop' and self.measures.stop:
+                stopped_state = state
+                state = 'stop'
+
+            if state == 'run':
+                if self.measures.visitingLed==True:
+                    state='wait'
+                if self.measures.ground==0:
+                    self.setVisitingLed(True);
+                self.wander()
+            elif state=='wait':
+                self.setReturningLed(True)
+                if self.measures.visitingLed==True:
+                    self.setVisitingLed(False)
+                if self.measures.returningLed==True:
+                    state='return'
+                self.driveMotors(0.0,0.0)
+            elif state=='return':
+                if self.measures.visitingLed==True:
+                    self.setVisitingLed(False)
+                if self.measures.returningLed==True:
+                    self.setReturningLed(False)
+                self.wander()
+            
+
+    def wander(self):
+        
+
+        center_id = 0
+        left_id = 1
+        right_id = 2
+        back_id = 3
+
+        if    self.measures.irSensor[center_id] > 5.0\
+           or self.measures.irSensor[left_id]   > 5.0\
+           or self.measures.irSensor[right_id]  > 5.0\
+           or self.measures.irSensor[back_id]   > 5.0:
+            print('Rotate left')
+            #self.driveMotors(-0.1,+0.1)
+            lpow = -0.1
+            rpow = +0.1
+            self.motors = (lpow, rpow) 
+            self.driveMotors(lpow,rpow)
+
+        elif self.measures.irSensor[left_id]> 2.7:
+            print('Rotate slowly right')
+            #self.driveMotors(0.1,0.0)
+            lpow = 0.1
+            rpow = 0.0
+            self.motors = (lpow, rpow)
+            self.driveMotors(lpow,rpow)
+
+        elif self.measures.irSensor[right_id]> 2.7:
+            print('Rotate slowly left')
+            #self.driveMotors(0.0,0.1)
+            lpow = 0.0
+            rpow = 0.1
+            self.motors = (lpow, rpow)
+            self.driveMotors(lpow,rpow)
+
+        else:
+            #$print('Go')
+            if (self.measures.stop) :
+                lpow = 0.0
+                rpow = 0.0
+                self.motors = (lpow, rpow)
+                self.driveMotors(self.motors)
+            else:
+                if self.firstRun:                               # Primeiro movimento
+                    if self.x is not None:
+                        self.initialx = self.x                              # Receber coordenada X do GPS
+                        print(self.initialx )
+                    if self.y is not None:
+                        self.initialy = self.y                              # Receber coordenada Y do GPS
+                        print(self.initialy )
+
+                    if self.y is not None and self.x is not None:
+                        self.firstRun = 0                                   # Já deu o arranque
+
+                # real pos
+                if self.firstRun == 0:                          # Se já arrancou
+                    self.posx = self.x - self.initialx + 5                  # Coordenada X em relação á posicao inicial
+                    self.posy = -(self.y - self.initialy + (6-14))          # Coordenada Y em relação à posição inicial 
+
+                #print(f'Xg: {self.posx:.2f}    Yg: {self.posy:.2f}    thetag: {self.ori}')
+
+                lpow = 0.1
+                rpow = 0.1
+                self.motors = (lpow, rpow) 
+                self.driveMotors(lpow,rpow)                     # Andar com velocidade constante (L = 0.1, R = 0.1)
+
+        
+        sens = list(map(int, self.measures.lineSensor))         # Linha de Sensores
+
+        if self.i < 5:
+            self.inputfilter.append(sens)                       # Buffer Leitura linha de sensores
+            self.i += 1
+        else:
+            self.inputfilter.pop(0)
+            self.inputfilter.append(sens)
+        total = 0
+
+        for i in range(self.i-1):
+            total += sum(self.inputfilter[i])                   # Soma de 1's presentes no buffer
+        #print(f"Buffer sensor de linha: {total}")
+
+        if total > 10:
+            flag_loc = 1
+        else:
+            flag_loc = 0
+
+        
+
+        self.odometry_move(self.motors)                         # Movimento calculado por odometria
+        self.particulas.odometry_move(self.motors)              # Mover particulas               
+        self.particulas.w_calc(flag_loc)                        # Calcular pesos de cada particula
+        self.particulas.w_norm()                                # Normalizar peso de cada particula
+        self.particulas.resample()                              # Resample de particulas
+        
+        # Imagem Open CV
+        self.particulas.clearImg()                              # Limpar imagem visualizada
+        self.particulas.drawMap()                               # Desenhar mapa 
+        self.particulas.drawReal(self.posx,self.posy,self.ori)  # Desenhar posição real do robot
+        self.particulas.drawParticles()                         # Desenhar todas as particulas
+        self.particulas.showImg()                               # Mostrar imagem
+                
+
+    def odometry_move(self, motors):
+        
+        self.motors = motors        
+
+        # calculate estimated power apply
+        out_l = (self.motors[0] + self.last_motors[0]) / 2
+        out_r = (self.motors[1] + self.last_motors[1]) / 2
+        
+        #out_l = random.gauss(out_l, 0.0015*out_l)   # out_l tem um erro de 1,5%
+        #out_r = random.gauss(out_r, 0.0015*out_r)    # out_r tem um erro de 1,5%
+        if out_l > 0.15:
+            out_l = 0.15
+        if out_r > 0.15:
+            out_r = 0.15
+            
+        # pos
+        lin = (out_l + out_r) / 2
+        x = self.x_od_pos + 2*(lin * cos(radians(self.ori)))
+        y = self.y_od_pos - 2*(lin * sin(radians(self.ori)))
+        
+        rot = (out_r - out_l) / self.robot_diameter         # self.robot_diameter = 1 
+        self.ori = degrees(radians(self.ori) + rot) % 360
+        ori = self.ori
+        #print(f'x0: {x:.2f}    y0: {y:.2f}    theta0: {ori}' )
+
+        self.x_od_pos = x # Posicao X por odometria
+        self.y_od_pos = y # Posicao Y por odometria
+
+
+            
+
+class Map():
+    def __init__(self, filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        
+        self.labMap = [[' '] * (CELLCOLS*2-1) for i in range(CELLROWS*2-1) ]
+        i=1
+        for child in root.iter('Row'):
+           line=child.attrib['Pattern']
+           row =int(child.attrib['Pos'])
+           if row % 2 == 0:  # this line defines vertical lines
+               for c in range(len(line)):
+                   if (c+1) % 3 == 0:
+                       if line[c] == '|':
+                           self.labMap[row][(c+1)//3*2-1]='|'
+                       else:
+                           None
+           else:  # this line defines horizontal lines
+               for c in range(len(line)):
+                   if c % 3 == 0:
+                       if line[c] == '-':
+                           self.labMap[row][c//3*2]='-'
+                       else:
+                           None
+               
+           i=i+1
+
+
+rob_name = "pClient1"
+host = "localhost"
+pos = 1
+mapc = None
+
+for i in range(1, len(sys.argv),2):
+    if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
+        host = sys.argv[i + 1]
+    elif (sys.argv[i] == "--pos" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
+        pos = int(sys.argv[i + 1])
+    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-r") and i != len(sys.argv) - 1:
+        rob_name = sys.argv[i + 1]
+    elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
+        mapc = Map(sys.argv[i + 1])
+    else:
+        print("Unkown argument", sys.argv[i])
+        quit()
+
+if __name__ == '__main__':
+    rob=MyRob(rob_name,pos,[0.0,60.0,-60.0,180.0],host)
+    if mapc != None:
+        rob.setMap(mapc.labMap)
+        rob.printMap()
+    
+    rob.run()
---
+++ pClient/particleFilter.py
@@ -0,0 +1,183 @@
+#import particles
+import numpy as np
+from math import *
+import random
+import cv2
+
+class particula():
+    def __init__(self, x, y, ori, w):
+        self.x = x
+        self.y = y
+        self.ori = ori
+        self.weight = w
+
+class filtroParticulas():
+    def __init__(self,n_part=2000, mapmax_x=28, mapmax_y=14):
+        self.n_part = n_part
+        self.s_w = n_part
+        self.max_w = 1
+        self.last_motors = (0,0)
+        self.sum_square_w = 0
+        self.particulas = []
+        self.mapmax_x = mapmax_x
+        self.mapmax_y = mapmax_y
+        self.norm_weights = []
+
+        for i in range (self.n_part):
+            self.particulas.append(particula(random.random() * ((mapmax_x-1)+0.5), random.random() * (mapmax_y-1)+0.5, random.random()*360 - 180, 1))
+
+            #self.particulas.append((random.random() * ((mapmax_x-1)+0.5), random.random() * (mapmax_y-1)+0.5, random.random()*360 - 180, 1))
+            #self.particulas.append((5, 8, 0))
+            
+            #self.weights.append(1)
+            self.norm_weights.append(1/self.n_part)
+            #self.ori.append(self.particulas[i][-1])
+            #self.ori.append(0)
+
+        # Tamanho imagem = 28 x 14 -> 1120 x 560 = simulação*40         x40  
+        self.immax_x = 40 * self.mapmax_x
+        self.immax_y = 40 * self.mapmax_y
+        self.img = np.zeros((self.immax_y,self.immax_x,3), np.uint8)
+
+    def odometry_move(self, motors):       
+        self.motors = motors
+
+        for i,v in enumerate (self.particulas):
+            # calculate estimated power apply
+            out_l = (self.motors[0] + self.last_motors[0]) / 2
+            out_r = (self.motors[1] + self.last_motors[1]) / 2
+            
+            out_l = random.gauss(out_l, 0.075*out_l)   # out_l tem um erro de 1,5%
+            out_r = random.gauss(out_r, 0.075*out_r)    # out_r tem um erro de 1,5%
+
+            if out_l > 0.15:
+                out_l = 0.15
+
+            if out_r > 0.15:
+                out_r = 0.15
+            
+            # pos
+            lin = (out_l + out_r) / 2
+            x = v.x + (lin * cos(radians(v.ori)))
+            y = v.y - (lin * sin(radians(v.ori)))
+
+            rot = out_r - out_l # / self.robot_diameter ( = 1 )
+            ori = degrees(radians(v.ori) + rot) % 360
+
+
+            v.x = x
+            v.y = y
+            v.ori = ori
+            
+            
+            self.last_motors = (out_l,out_r)
+        
+
+    def resample(self):
+        #print(f'sum(S_W**2) -> {self.sum_square_w}')
+        print(1./self.sum_square_w)
+        n = 0.95 * self.n_part
+        if (1./self.sum_square_w < n):
+        #if False:
+        #if True:
+            print("---------- ReSampling!!!!!- -----------")
+            indices = []
+            C = [0.] +[sum(self.norm_weights[:i+1]) for i in range(self.n_part)]
+            u0, j = random.random(), 0
+
+            for u in [(u0+i)/self.n_part for i in range(self.n_part)]:
+                while u > C[j]:
+                    j+=1
+
+                indices.append(j-1)
+
+            newParticles = []
+
+            for i,v in enumerate(indices):
+                newParticles.append(particula(self.particulas[v].x, self.particulas[v].y, self.particulas[v].ori, self.particulas[v].weight))
+                newParticles[i].weight = 1
+          
+            self.particulas = newParticles
+
+    def w_calc(self, flag_loc):
+        self.max_w = 1     
+        for i,v in enumerate(self.particulas):
+            if (v.x > self.mapmax_x-0.4 or v.x < 0+0.4 or v.y > self.mapmax_y-0.4 or v.y < 0+0.4):   # Está fora do mapa
+                out_o_b = True
+                v.weight = 0.5
+
+            else: # Está dentro do mapa
+                out_o_b = False
+                
+            if not out_o_b:        # Estando dentro do mapa
+                if flag_loc == 1:       # Robot real Dentro da area
+                    if ( (v.x < 20 and v.x>13.5 and v.y < 9.5 and v.y > 4) or (v.x > 4.5 and v.x < 10 and v.y < 9.5 and v.y > 4) ):   # Particula dentro da area
+                        v.weight += 1
+
+                    else:                   # Particula fora da area
+                        v.weight -= 5
+
+                else:               # Robot real Fora da area
+                    if ( (v.x < 20 and v.x>13.5 and v.y < 9.5 and v.y > 4) or (v.x > 4.5 and v.x < 10 and v.y < 9.5 and v.y > 4) ):     # Particula fora da area
+                        v.weight -= 5
+                        
+                    else:
+                        v.weight += 1
+            if v.weight < 1:
+                v.weight = 0.5
+
+            if v.weight > self.max_w:
+                self.max_w = v.weight
+            
+    def w_norm(self):
+        s_w = 0
+        ssw = 0
+        
+        for i,v in enumerate(self.particulas):
+            s_w += v.weight
+
+        self.s_w = s_w
+        
+        
+        for i,v in enumerate(self.particulas): 
+            self.norm_weights[i] = v.weight/self.s_w
+            ssw += self.norm_weights[i]**2 
+
+        self.sum_square_w = ssw
+ 
+    def drawParticles(self):
+        print(self.max_w)
+
+        for i,v in enumerate(self.particulas):
+            x = int(40*v.x)
+            y = int(40*v.y)
+            ori = v.ori
+
+            if v.weight < 1:
+                cv2.circle(self.img, (x,y), 5, (0,0,255), -1)
+                cv2.line( self.img, (x,y), (x+int(10*cos(radians(ori))), y-int(10*sin(radians(ori)))), (255,0,0),2)
+
+            else:
+                cv2.circle(self.img,(x,y), 5, (0,255,0), -1)
+                cv2.line( self.img, (x,y), (x+int(10*cos(radians(ori))), y-int(10*sin(radians(ori)))), (255,0,0),2)
+
+    def drawReal(self,x,y,ori):
+        cx = int(40*x)
+        cy = int(40*y)
+        
+        cv2.circle(self.img,(cx,cy), 20, (150,150,0), -1) # Circulo centrado no centro do robot real
+        cv2.line( self.img, (cx,cy), (cx+int(20*cos(radians(ori))), cy-int(20*sin(radians(ori)))), (255,0,0),2) # Linha do centro do robot direcionada segundo orientaçao
+        #print(f'\nGPS: x: {40*x+40*cos(ori)}   y: {40*y+40*sin(ori)}   theta: {ori}')
+
+    def drawMap(self):
+        cv2.rectangle(self.img,(int(40*13.5),40*4),(40*20,int(40*9.5)),(255,0,0),-1)
+        cv2.rectangle(self.img,(int(40*4.5),40*4),(40*10,int(40*9.5)),(255,0,0),-1)
+
+
+    def showImg(self):
+        cv2.imshow("img",self.img)
+        cv2.waitKey(25)
+
+    def clearImg(self):
+        self.img = np.zeros((560,1120,3), np.uint8)
+
